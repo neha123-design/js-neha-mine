cfc46645adbcc7b3b9acf8913cca982da43c1f44
neha123-design <2025ugee072@nitjsr.ac.in>
2026-05-22 14:42:32 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+// alert(3+3) we cant use it here as we are using node js here not browser
+//code readability should be high
+console.log(3+3)
+console.log("hitesh")
+let name="neha";
+let age=14;
+let islogedin=false;
+
+//range of number is 2 to power 53
+// if number is large then we use "bigint"
+//string => use in double quote most recommended
+//boolean=>true/false
+//null =>stand alone value or it's empty
+//undefined => variable defined but doesn't assign any value
+//symbol=>unique
+
+//object
+
+console.log(typeof(age));
+//null is an object not any number type and undefined 
+//is type of undefined
+console.log(typeof(null));
+console.log(typeof(undefined));
