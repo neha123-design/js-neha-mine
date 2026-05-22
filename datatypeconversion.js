981a887e2cc8feafc8c8c53103cb86cd191e0a31
neha123-design <2025ugee072@nitjsr.ac.in>
2026-05-22 15:38:03 +0530

    my datatypeconversion

---
+++ datatypeconversion.js
@@ -0,0 +1,23 @@
+let number=33;
+console.log(typeof(number))
+let str=String(number);
+console.log(typeof str);
+console.log(str);
+let s="";
+let valuetonumber=Number(s);
+console.log(valuetonumber);
+
+
+// "33"=>33
+//"33abc"=>NaN     true=>1    false=>0
+
+
+
+let boole="";
+let booleantovalue=Boolean(boole);
+console.log(booleantovalue);
+
+//number to boolean 
+
+
+
